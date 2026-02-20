824c48ceb6bebf97c5c087bd2928a7cc077c25d9
Nakulan <nakulan07022007@gmail.com>
2026-02-20 17:27:53 +0530

    Fixed the fileS

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
+require("dotenv").config();
+
 const questionRoutes = require("./routes/questions");
 const authRoutes = require("./routes/auth");
 const discussionRoutes = require("./routes/discussions");
 
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/fsd_project")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+// Root Route for testing deployment
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 
 // Use Routes
 app.use("/api/questions", questionRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/discussions", discussionRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Server Startup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
