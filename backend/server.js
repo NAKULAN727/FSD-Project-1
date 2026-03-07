693472ac8fc94b0bf6990d71f4bac4ef9c628075
Nakulan <nakulan07022007@gmail.com>
2026-03-07 10:21:24 +0530

    Added the URLs for vercel and render

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    origin: [
+      process.env.CLIENT_URL,
+      "http://localhost:5173",
+      "https://fsd-project-rust.vercel.app"
+    ],
     credentials: true,
   }),
 );
